7335a8b944934452bef01f8f74c6dfc8bf0e26ce
netlify736-dotcom <netlify736@gmail.com>
2026-09-12 17:06:15 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
     viteReact(),
+    netlify(),
   ],
 })
